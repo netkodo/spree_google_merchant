37aa07fc03044f3c59a40e9d11659ffbb7a10c42
Justin Mui <justin.mui@thereformation.com>
2015-02-03 15:57:28 -0800

    - removing other unnecessary routes and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Spree::Core::Engine.routes.append do
   namespace :admin do
     resource :google_merchant_settings
-    resources :product_ad_channels do
-      resources :product_ads
-    end
-    resources :product_ads
-
-    get '/products/:product_id/product_ads', :to => 'product_ads#index'
+#    resources :product_ad_channels do
+#      resources :product_ads
+#    end
+#    resources :product_ads
+#
+#    get '/products/:product_id/product_ads', :to => 'product_ads#index'
   end
 end
--- db/migrate/20140912004237_add_max_cpc_to_spree_variants.rb
+++
@@ -1,5 +0,0 @@
-class AddMaxCpcToSpreeVariants < ActiveRecord::Migration
-  def change
-    add_column :spree_variants, :max_cpc, :decimal, :precision => 8, :scale => 2, :default => nil
-  end
-end
