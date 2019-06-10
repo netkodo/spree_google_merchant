a68763d9d43f1adf3f0f06748fe3294139f9289f
Daniel Niedzialkowski <dniedzialkowski@netkodo.com>
2019-06-10 16:13:09 +0200

    changed order type

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -24,7 +24,7 @@ Spree::Variant.class_eval do
   end
 
   def google_merchant_gtin
-    upcs = self.property_variants.order(created_at: :desc).select{|x| x.property.name == 'upc'}
+    upcs = self.property_variants.order(updated_at: :desc).select{|x| x.property.name == 'upc'}
     if upcs.present?
       upc = upcs.first
       upc.value
